212c26a3c66e1f2588866a0f9616d0982d19fece
PauloJPS <paulo.paulino.souza96@gmail.com>
2019-06-19 08:17:32 -0300

    plot inhomogeneous networks

--- networks.py
+++ networks.py
@@ -3,6 +3,74 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 
+
+def plotInemoChains():
+    g = ig.Graph.Watts_Strogatz(1, 12, 1, 0)
+    pos = g.layout_circle()
+    gnx = ig2nx(g)
+
+    fig, ax = plt.subplots(2, 2)
+
+    ax1 = ax[0]
+    ax2 = ax[1]
+    
+    nx.draw_networkx_nodes(gnx, pos, nodelist=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 
+                            node_shape=r'$\uparrow$', node_size=500, node_color='red', ax=ax1[0])
+    nx.draw_networkx_nodes(gnx, pos, nodelist=[0], 
+                            node_shape=r'$\downarrow$', node_size=1000, node_color='green', ax=ax1[0])
+    #nx.draw_networkx_edges(gnx, pos, ax=ax1[0])
+
+ 
+    nx.draw_networkx_nodes(gnx, pos, nodelist=[1, 3, 4, 5, 6, 7, 8, 9, 10, 11], 
+                            node_shape=r'$\uparrow$', node_size=500, node_color='red', ax=ax1[1])
+    nx.draw_networkx_nodes(gnx, pos, nodelist=[0, 2], 
+                            node_shape=r'$\downarrow$', node_size=1000, node_color='green', ax=ax1[1])
+    #nx.draw_networkx_edges(gnx, pos, ax=ax1[1])
+
+
+    nx.draw_networkx_nodes(gnx, pos, nodelist=[1, 3, 5, 7, 9, 11], 
+                            node_shape=r'$\uparrow$', node_size=500, node_color='red', ax=ax2[0])
+    nx.draw_networkx_nodes(gnx, pos, nodelist=[0, 2, 4, 6, 8, 10], 
+                            node_shape=r'$\downarrow$', node_size=1000, node_color='green', ax=ax2[0])
+    #nx.draw_networkx_edges(gnx, pos, ax=ax2[0])
+
+
+    nx.draw_networkx_nodes(gnx, pos, nodelist=[11], 
+                            node_shape=r'$\uparrow$', node_size=500, node_color='red', ax=ax2[1], label='Spin 1/2')
+    nx.draw_networkx_nodes(gnx, pos, nodelist=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 
+                            node_shape=r'$\downarrow$', node_size=1000, node_color='green', ax=ax2[1], label='Spins 1')
+    #nx.draw_networkx_edges(gnx, pos, ax=ax2[1])
+
+    ax2[1].legend(loc=1, bbox_to_anchor=(0.2, 2.5), markerscale=0.5, fontsize=12)
+
+    
+    ax1[0].set_axis_off()
+    ax1[1].set_axis_off()
+        
+    ax2[0].set_axis_off()
+    ax2[1].set_axis_off()
+    
+    ax1[0].set_xlim((-1.3, 1.3))
+    ax1[0].set_ylim((-1.3, 1.3))
+
+    ax1[1].set_xlim((-1.3, 1.3))
+    ax1[1].set_ylim((-1.3, 1.3))
+
+    ax2[0].set_xlim((-1.3, 1.3))
+    ax2[0].set_ylim((-1.3, 1.3))
+
+    ax2[1].set_xlim((-1.4, 1.4))
+    ax2[1].set_ylim((-1.4, 1.4))
+
+    ax1[0].set_title(r'$N_i = 1 $')
+    ax1[1].set_title(r'$N_i = 2 $')
+        
+    ax2[0].set_title(r'$N_i = 6$')
+    ax2[1].set_title(r'$N_i = 11 $')
+
+    #plt.tight_layout()
+     
+
 def plotNetwor():
     g1 = ig.Graph.Watts_Strogatz(1, 15, 1, 0)
     pos1 = g1.layout_circle()
